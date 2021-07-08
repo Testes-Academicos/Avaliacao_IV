cbdc3eeff4aaea3cf953ff6dfcee273ca0a732df
DucilioManjate <liomanjate@gmail.com>
2021-07-08 09:12:23 -0300

    first commit

---
+++ src/test/java/com/uniamerica/prova4/Prova4ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.uniamerica.prova4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova4ApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+        System.out.printf("hello world");
+    }
+
+}
